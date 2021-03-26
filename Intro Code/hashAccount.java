cd6914a918e21da8744caefc40f0f2a98421594e
borisrokhmanov <70663795+borisrokhmanov@users.noreply.github.com>
2021-03-26 10:27:33 -0500

    3-26-2021
    hashing acc stuff

---
+++ Intro Code/hashAccount.java
@@ -0,0 +1,31 @@
+//boris rokhmanov
+//hash account lab
+
+public class hashAccount
+{
+    Account p1 = new Account ("Homer Simpson" ,0001, 1090.45, 1);
+    Account p2 = new Account ("Megan Brimstead" ,0002, 786.54, 1);
+    Account p3 = new Account ("Liz Wilson" ,1349, 2034.52, 1);
+    Account p4 = new Account ("Homer Simpson" ,0001, 1090.45, 1);
+    Account p5 = new Account ("Jack Hagested" ,2389, 280.67, 1);
+    Account p6 = new Account ("Bill Hagested" ,2391, 1008.53, 1);
+    Account p7 = new Account ("Emy Hayes" ,3598, 1260.84, 1);
+    Account p8 = new Account ("Doug Dralle" ,4562, 5783.90, 1);
+    Account p9 = new Account ("Eric Wilhelm" ,3208, 3420.75, 1);
+    Account p10 = new Account ("Megan MulCahey" ,4568, 9041.49, 1);
+    
+    Account[] people = {p1, p2, p3, p4, p5, p6, p7, p8, p9, p10};
+    
+    public int hashCode(){
+        int code = 0;
+        return code;
+    }
+    
+    //public Account[] sorted(){
+        
+    //}
+    
+    public static void main(String args[]){
+        
+    }
+}
